99f7d9ba604c6f4857e753200906e308e9c10ca3
mayumi kawanami <mayumi.19821215@gmail.com>
2024-06-13 18:47:38 +0900

    cronのテスト実行

--- src/app/Http/Requests/RatingRequest.php
+++ src/app/Http/Requests/RatingRequest.php
@@ -24,7 +24,7 @@ class RatingRequest extends FormRequest
     public function rules()
     {
         return [
-            'reservation_id' => 'required|exists:reservations,id',
+            'reservation_id' => 'required',
             'rating' => 'required|integer|min:1|max:5',
             'comment' => 'nullable|string|max:255',
         ];
@@ -33,8 +33,7 @@ class RatingRequest extends FormRequest
     public function messages()
     {
         return [
-            'reservation_id.required' => '予約を選択してください',
-            'reservation_id.exists' => '選択した予約は存在しません',
+            'reservation_id.required' => '来店済みの予約を選択してください',
             'rating.required' => '評価を選択してください',
             'rating.integer' => '評価は整数である必要があります',
             'rating.min' => '評価は1以上である必要があります',
