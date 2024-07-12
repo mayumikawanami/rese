9ec9e65f5ecced939c5a835492e41d675d24bf7a
mayumi kawanami <mayumi.19821215@gmail.com>
2024-07-13 01:16:50 +0900

    fix:DatabeseSeederのrunの順序修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(AreasTableSeeder::class);
+        $this->call(GenresTableSeeder::class);
         $this->call(ShopsTableSeeder::class);
         $this->call(RolesAndPermissionsSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(AreasTableSeeder::class);
-        $this->call(GenresTableSeeder::class);
     }
 }
